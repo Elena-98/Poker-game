e009d91c6ff23be3ff50a4cc1082f3a7df201210
Elena-98 <xue12@student.unimelb.edu.au>
2024-03-08 12:38:23 +1100

    declare card value

---
+++ src/PokerOrder.java
@@ -0,0 +1,13 @@
+public class PokerOrder {
+
+    private static final String VALUES = "23456789TJQKA";
+    private static final String SUITS = "DHSC";
+    private String[] cards;
+    public PokerOrder (String[] cards){
+        this.cards = cards;
+
+    }
+
+    private void combination(){}
+
+}
