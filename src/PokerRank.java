2c210ab1f45af3a598370f4dd2f93b908a87ad1c
Elena-98 <xue12@student.unimelb.edu.au>
2024-03-09 14:14:27 +1100

    read txt file

--- src/PokerRank.java
+++ src/PokerRank.java
@@ -1,4 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PokerRank {
+
+    public static void main (String[] args) throws FileNotFoundException {
+        String filePath = "src/poker-hands.txt";
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Failed to read file: " + e.getMessage());
+        }
+    }
 
 
 }
